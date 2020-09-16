6c00cfc464b9cde546719da1c511e6719f2b58da
Toshio Sekiya <lxboyjp@gmail.com>
2020-09-16 21:17:48 +0900

    Change the directory to which ltx is installed, because of the updated ruby version.

--- ltx/install.sh
+++ ltx/install.sh
@@ -1,3 +1,6 @@
 # ltx.rbを$LOAD_PATHで指定されているフォルダにコピー
+#
+# $LOAD_PATHのディレクトリを調べるには、次のコマンドを実行すれば良い。
+# $ ruby -e 'puts $:'
 
-cp ltx.rb /home/toshio/.rbenv/versions/2.6.0/lib/ruby/site_ruby/2.6.0/x86_64-linux
+cp ltx.rb /home/toshio/.rbenv/versions/2.7.1/lib/ruby/site_ruby/2.7.0/x86_64-linux
--- ltx/uninstall.sh
+++ ltx/uninstall.sh
@@ -1,3 +1,3 @@
 # ltx.rbを$LOAD_PATHで指定されているフォルダから削除
 
-rm /home/toshio/.rbenv/versions/2.6.0/lib/ruby/site_ruby/2.6.0/x86_64-linux/ltx.rb
+rm /home/toshio/.rbenv/versions/2.7.1/lib/ruby/site_ruby/2.7.0/x86_64-linux/ltx.rb
